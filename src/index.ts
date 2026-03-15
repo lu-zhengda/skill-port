43271a2c3e7881b2799bea788b71b062c953fa33
Zhengda Lu <zl2301@columbia.edu>
2026-03-15 10:21:27 -0400

    feat: export plugin discovery functions from public API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 export { convertSkill, convertScopedSkill, formatTextReport, StrictModeError } from "./core/convert.js";
 export { formatSkillsText, listSkillsByScope } from "./core/list.js";
+export { listPluginSkills, readInstalledPlugins } from "./core/plugins.js";
 export type {
   CanonicalSkill,
   ConvertOptions,
   ConvertResult,
   ConversionReport,
+  InstalledPluginsFile,
   ListedSkill,
+  PluginInstall,
   Provider,
   Scope,
   ScopedConvertOptions,
